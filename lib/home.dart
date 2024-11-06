e9a6428c8a1467854fdfdbc193dde08e11c3cab6
Mashan <muhrayhan582@gmail.com>
2024-11-06 18:40:14 +0700

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -12,22 +12,40 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        title: const Text(
-          "Muhammad Rayhan",
-          style: TextStyle(color: Colors.black),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Bagian kiri untuk ikon profil dan teks
+            Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Mengatur ikon dan teks ke kiri
+              children: [
+                const Icon(Icons.account_circle, color: Colors.black, size: 28),
+                const SizedBox(height: 4), // Jarak antara ikon dan teks
+                const Text(
+                  "Muhammad Rayhan",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            // Bagian kanan untuk ikon notifikasi dan pengaturan
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.notifications, color: Colors.black),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.settings, color: Colors.black),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
         ),
-        centerTitle: false,
-        leading: const Icon(Icons.account_circle, color: Colors.black),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications, color: Colors.black),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.settings, color: Colors.black),
-            onPressed: () {},
-          ),
-        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -35,11 +53,22 @@ class HomePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 16), // Adjust spacing if needed
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Text("Widget Grid View"),
+              const SizedBox(height: 16),
+              // Grid View Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Widget Grid View",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward, color: Colors.black),
+                    onPressed: () {},
+                  ),
+                ],
               ),
+              const SizedBox(height: 8),
               SizedBox(
                 height: 150,
                 child: ListView.builder(
@@ -47,22 +76,41 @@ class HomePage extends StatelessWidget {
                   itemCount: 5,
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      padding: const EdgeInsets.symmetric(horizontal: 3.0),
                       child: Card(
-                        child: SizedBox(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Container(
+                          padding: const EdgeInsets.all(3.0),
                           width: 120,
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Icon(
-                                Icons
-                                    .music_note, // Replace with your asset icons
-                                size: 50,
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  ),
+                                  child: const Center(
+                                    child: Icon(Icons.image, size: 60),
+                                  ),
+                                ),
                               ),
-                              Text(
-                                  "Artist $index"), // Replace with actual artist names
-                              Text(
-                                  "Song $index"), // Replace with actual song names
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, bottom: 8.0, top: 4.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text("Artist",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                    Text("Song"),
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
                         ),
@@ -71,24 +119,67 @@ class HomePage extends StatelessWidget {
                   },
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Text("Widget List View"),
+              const SizedBox(height: 8),
+              // List View Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Widget List View",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward, color: Colors.black),
+                    onPressed: () {},
+                  ),
+                ],
               ),
+              const SizedBox(height: 16),
               ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
                 itemCount: 6,
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
                     child: ListTile(
                       contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 8.0),
-                      leading: const Icon(Icons.music_note),
-                      title: Text("Headline $index"),
-                      subtitle: Text("Description $index"),
-                      trailing: const Icon(Icons.play_arrow),
+                          const EdgeInsets.symmetric(horizontal: 12.0),
+                      leading: Container(
+                        width: 150.0,
+                        height: 150.0,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: const Icon(Icons.image, size: 60.0),
+                      ),
+                      title: const Text(
+                        "Headline",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Description duis aute irure dolor in reprehenderit in voluptate velit...",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 4),
+                          Row(
+                            children: [
+                              const Icon(Icons.add_circle_outline, size: 16),
+                              const SizedBox(width: 4),
+                              const Text("Today"),
+                              const SizedBox(width: 8),
+                              const Text("23 min"),
+                              const Spacer(),
+                              const Icon(Icons.play_arrow),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
